4daa4427c8d06980d16fea81b51f6fc52b1ca41c
Deise Araújo <127044115+DehAraujo@users.noreply.github.com>
2026-03-12 06:01:25 +0000

    Remove arquivos do exercicio4 do exercicio5

--- Exercicio05/tests/test_completo.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from src.teste_completo import analisar
-
-#teste completo
-@pytest.mark.parametrize("numero, resultado_esperado", [
-
-])
-def test_completo_analisar(numero, resultado_esperado):
-    assert analisar(numero) == resultado_esperado
